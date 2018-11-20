ec4bd1ff998f3c80cd2696b4fc481b2f056efeb6
rajju <vajrachara@yahoo.com>
2018-11-20 03:11:03 +0000

    final upload but image detection need to rectify

--- server.js
+++ server.js
@@ -2,7 +2,27 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin   = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'face-detection-db'
+  }
+});
+
+
+
+console.log(db.select('*').from('users'));
 
 const app = express();
 
@@ -30,65 +50,22 @@ database = {
   ]
 }
 
-app.get('/',(req, res) => {
-  res.json(database.users);
-})
+app.get('/',(req, res) => {res.json(database.users);} )
 
-app.post('/signin', (req, res) => {
-  if( req.body.email === database.users[0].email
-      && req.body.password === database.users[0].password){
-        //res.json('success');
-        res.json(database.users[0]);
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
-      }else{
-        res.status(400).json('login fail');
-      }
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
 
-app.post('/register', (req, res) => {
-  const { user, name, email, password } = req.body;
-  database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-  })
-  res.json(database.users[database.users.length-1]);
-})
+app.get('/profile/:id', (req, res ) => {profile.handleProfileGet(req, res, db)} )
 
-app.get('/profile/:id', (req, res) => {
-  const {id} = req.params;
-  let found = false;
-  database.users.forEach( user => {
-    found = true;
-    if(user.id === id){
-      return res.json(user);
-    }else{
-      return res.status(404).json('user not found');
-    }
-  })
-  // if(!found){
-  //    res.status(404).json('user not found');
-  // }
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)} )
 
-app.put('/image', (req,res) => {
-      const {id} = req.body;
-      let found = false;
-      database.users.forEach( user => {
-        found = true;
-        if(user.id === id){
-          user.entries++;
-          return res.json(user.entries);
-        }
-      })
-      if(found){
-         res.status(404).json('user not found');
-      }
-})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)} )
 
 
 app.listen(5000, () =>{
   console.log('app is listening at port 5000');
 })
+
+
+console.log(process.env);
