75fb20273abaa01105bfcfda257b0085016b05f7
rajju <vajrachara@yahoo.com>
2018-11-21 05:03:46 +0000

    updates

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyparser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
 const register = require('./controllers/register');
 const signin   = require('./controllers/signin');
 const profile = require('./controllers/profile');
